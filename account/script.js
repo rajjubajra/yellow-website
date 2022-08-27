415dc98cf9e5df4e238dfb7bdf1046024e131992
rajjubajra <rajjubajra@gmail.com>
2022-08-27 19:51:03 +0100

    script.js file updated

--- account/script.js
+++ account/script.js
@@ -1,4 +1,8 @@
-fetch("https://yellow-website.com/yw-d9/jsonapi/node/invoice?include=field_bank_account") // first step
+fetch("https://yellow-website.com/yw-d9/jsonapi/node/invoice", {
+  headers: {
+    Accept: "application/vnd.api+json",
+  }
+}) // first step
   .then(response => response.json()) // second step
   .then(data => {
     console.log(data)
--- yw-d9/account/script.js
+++
@@ -1,6 +0,0 @@
-fetch("https://yellow-website.com/yw-d9/jsonapi/node/invoice?include=field_bank_account") // first step
-  .then(response => response.json()) // second step
-  .then(data => {
-    console.log(data)
-  })
-  .catch(error => console.error(error))
