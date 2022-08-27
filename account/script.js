8b476f854d61c087343d73e3224088e0b4347df9
rajjubajra <rajjubajra@gmail.com>
2022-08-27 16:48:49 +0100

    account scrpt page

--- account/script.js
+++ account/script.js
@@ -1,3 +1,6 @@
 fetch("https://yellow-website.com/yw-d9/jsonapi/node/invoice?include=field_bank_account") // first step
-  .then(response => console.log(response.json())) // second step
+  .then(response => response.json()) // second step
+  .then(data => {
+    console.log(data)
+  })
   .catch(error => console.error(error))
