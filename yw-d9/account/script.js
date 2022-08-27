d803c6ce5d1a02aaac4f7d2bf5ff1a7b6393e425
rajjubajra <rajjubajra@gmail.com>
2022-08-27 19:42:10 +0100

    accoutn moved to yw-d9

---
+++ yw-d9/account/script.js
@@ -0,0 +1,6 @@
+fetch("https://yellow-website.com/yw-d9/jsonapi/node/invoice?include=field_bank_account") // first step
+  .then(response => response.json()) // second step
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => console.error(error))
