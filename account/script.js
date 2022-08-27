7753c4772953d655bc2c51991006dba3eb018d90
rajjubajra <rajjubajra@gmail.com>
2022-08-27 19:54:16 +0100

    script.js file updated

--- account/script.js
+++ account/script.js
@@ -1,8 +1,4 @@
-fetch("https://yellow-website.com/yw-d9/jsonapi/node/invoice", {
-  headers: {
-    Accept: "application/vnd.api+json",
-  }
-}) // first step
+fetch("https://yellow-website.com/yw-d9/jsonapi/node/invoice") // first step
   .then(response => response.json()) // second step
   .then(data => {
     console.log(data)
