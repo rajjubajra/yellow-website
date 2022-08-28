22d0092671c7d68805198ddc52e478a89b0399d9
rajjubajra <rajjubajra@gmail.com>
2022-08-28 09:22:19 +0100

    script.js updated

--- account/script.js
+++ account/script.js
@@ -1,4 +1,4 @@
-fetch("https://yellow-website.com/yw-d9/jsonapi/node/invoice") // first step
+fetch("https://yellow-website.com/yw-d9/jsonapi/node/invoice?include=field_bank_account") // first step
   .then(response => response.json()) // second step
   .then(data => {
     console.log(data)
